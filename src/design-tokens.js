e804609d63f9649de454d947e225bc2becf7c939
Suhani <suhani4630@gmail.com>
2026-05-14 20:42:40 +0530

    test: full label coverage matrix
    Signed-off-by: Suhani <suhani4630@gmail.com>

--- src/design-tokens.js
+++ src/design-tokens.js
@@ -10,7 +10,7 @@ export const spacing = {
   xs: '0.5rem', // 8px
   sm: '1rem', // 16px
   md: '1.5rem', // 24px
-  lg: '2rem', // 32px
+  lg: '3rem', // 32px
   xl: '3rem', // 48px
   '2xl': '4rem', // 64px
   '3xl': '5rem', // 80px
